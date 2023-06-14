2b259876a4a33a950789059a96945b8872fc95db
Mitchell Duke <25DukMi@cc.catholic.edu.au>
2023-06-14 14:23:48 +0930

    Added Open Sans as font family

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,15 @@
 <script>
 	import { TextBox, Button, TextBlock } from 'fluent-svelte';
 	import 'fluent-svelte/theme.css';
-
-	let count = 0;
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+
+	* {
+		font-family: 'Open Sans';
+	}
+
 	/* Some base styles to get things looking right. */
 	:global(body) {
 		background-color: var(--fds-solid-background-base);
