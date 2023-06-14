b8cf86990f99899c3727fb10540982952bfe549c
Mitchell Duke <25DukMi@cc.catholic.edu.au>
2023-06-14 11:53:37 +0930

    Upload initial repository.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { TextBox, Button, TextBlock } from 'fluent-svelte';
+	import 'fluent-svelte/theme.css';
+
+	let count = 0;
+</script>
+
+<style>
+	/* Some base styles to get things looking right. */
+	:global(body) {
+		background-color: var(--fds-solid-background-base);
+		color: var(--fds-text-primary);
+	}
+</style>
+
+<div>
+	<form>
+		<h3>Reference a website</h3>
+		<p>Our tool will automatically query the site for it's data and it'll fill in the values below, if any are left empty it means the tool failed to find those types of data and you should enter them manually.</p>
+
+		<TextBox placeholder="Enter the site URL" />
+		<Button>Query data</Button>
+	</form>
+</div>
